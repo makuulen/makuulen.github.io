d592fdfe99e316502a79d19c5daab33b6a85b41b
makuulen <75946050+makuulen@users.noreply.github.com>
2021-01-13 12:09:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 	$.ajax({
 		type: "GET",
-		url: "http://site2.ru/counter.php?callback=?",
+		url: "http://teatama.hostronavt.ru/counter.php?callback=?",
 		dataType: 'jsonp',
 		success: function (data){
 			$('.text .counter').html(data.counter);
