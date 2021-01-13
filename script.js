8cc3e640ddb7945f3e71f5ebcfe6f2b9b28dae71
makuulen <75946050+makuulen@users.noreply.github.com>
2021-01-13 12:18:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
 		url: "http://teatama.hostronavt.ru/counter.php?callback=?",
 		dataType: 'jsonp',
 		success: function (data){
+                        alert(data.counter);
 			$('.text .counter').html(data.counter);
 		}
 	});
